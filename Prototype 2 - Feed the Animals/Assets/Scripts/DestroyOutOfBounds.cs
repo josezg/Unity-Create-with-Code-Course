f9019c3618d62cbe1a76b52d2847ea2f1e279fb9
Jose <josezgalvezz@gmail.com>
2019-12-30 23:53:12 -0500

    Unit 2 & Challenge 2 done!

---
+++ Prototype 2 - Feed the Animals/Assets/Scripts/DestroyOutOfBounds.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DestroyOutOfBounds : MonoBehaviour
+{
+    private float _topBound = 30.0f;//Position limit for projectiles
+    private float _lowerBound = -10.0f;//Position limit for animals
+    // Update is called once per frame
+    void Update()
+    {
+        if (transform.position.z > _topBound)//Condition for destroy projectiles that passed the top bound
+        {
+            Destroy(gameObject);
+        }
+        else if (transform.position.z < _lowerBound)//Condition for destroy animals that passed the bottom bound
+        {
+            Destroy(gameObject);
+            Debug.Log("Game Over!");
+        }
+    }
+}
---
+++ Prototype 2 - Feed the Animals/Assets/Scripts/SpawnManager.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnManager : MonoBehaviour
+{
+    public GameObject[] animalPrefabs;
+    private float spawnRangeX = 20.0f;
+    private float spawnPosZ = 20.0f;
+    private float startDelay = 2.0f;
+    private float spawnInterval = 1.5f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        InvokeRepeating("SpawnRandomAnimal",startDelay,spawnInterval);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void SpawnRandomAnimal()    
+    {
+        Vector3 spawnPos = GenerateRandomLocation();
+        GameObject animal = GenerateRandomAnimal();
+        Instantiate(animal, spawnPos,animal.transform.rotation);
+    }
+    Vector3 GenerateRandomLocation()
+    {
+        return new Vector3(Random.Range(-spawnRangeX,spawnRangeX),0,spawnPosZ);
+    }
+
+    GameObject GenerateRandomAnimal()
+    {
+        int animalIndex = Random.Range(0, animalPrefabs.Length);
+        return animalPrefabs[animalIndex];
+    }
+}
